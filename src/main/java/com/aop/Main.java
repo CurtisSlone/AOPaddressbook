7bfe5d46809b03b5ce2b780b7ff7c6ac51861e3f
Curtis Slone <curtisdslone@gmail.com>
2024-08-23 20:10:02 +0200

    deltas

--- src/main/java/com/aop/Main.java
+++ src/main/java/com/aop/Main.java
@@ -10,7 +10,7 @@ public class Main {
         new Contact("Gordon Ramsey", "567 Angry Chef Lane", "London", "NH", "65785", "245-234-4323"),
         new Contact("Chandler Bing", "56 Cup o Joe St", "New York", "NY", "89078", "718-123-9089"),
         new Contact("Barry Allen"," 549 Run Fast Way", "Baltimore", "MD", "09879", "235-345-4949"),
-        new Contact()
+        new Contact("Monica Smith", "12 Main St", "Baton Rouge", "LA", "70565", "234-435-7898")
         );
     public static void main(String[] args) {
         
